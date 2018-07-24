da0c160e6a3f23ebdb4ab21429a64fdbaf8363f9
Eric Jiang <eric@littlstar.com>
2018-07-24 11:29:30 -0400

    feat(): add library module

--- constants.js
+++ constants.js
@@ -3,9 +3,9 @@ module.exports = {
   kAidPrefix: 'did:ara:',
   kStorageAddress: '0xd38bbd737e01915f8c57ceab24790610b88b2369',
   kPriceAddress: '0xe7d6c54cf8bd798eda9e9a3aa094fb01ef34c251',
-  kPurchaseAddress: '0x320bc17afe2788da8215106eaa4feed766a66e74',
-  kLibraryAddress: '0xab6320a328a6716294c57eed68983df8c9591ee9',
+  kPurchaseAddress: '0x52b552330a421df6677b0c96967b1b3b703160a4',
+  kLibraryAddress: '0x3f47041f0da4c6ae697349b8068959a46c9a9557',
 
   kTempOwnerDid: 'did:ara:e3a808f2deba17c1dcbaf176b2c529cac80c71b8418fe123439a054f88ae2cd2',
-  kTempAFSDid: 'did:ara:2e6768840c7ad95c027437cf3d8ada2068f6a3b45fb0353486ba91dff28d5546'
+  kTempAFSDid: 'did:ara:7bfec1111365428137902d0152681a80f4ce535242ea384481cfc432e3016192'
 }
