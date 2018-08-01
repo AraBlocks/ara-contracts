565c26755a9fe6f255b1c1240c1bf8989643d220
Eric Jiang <eric@littlstar.com>
2018-08-01 15:58:12 -0400

    feat(): registry and proxy progress

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,12 +1,14 @@
 pragma solidity 0.4.24;
 
 import "./Proxy.sol";
+import "./OwnedUpgradabilityProxy.sol";
 
 contract Registry {
   address public owner_;
-  mapping (string => address) private proxies_; // contentId => proxy contract address
-  address public standard_;
-  string public version_;
+  mapping (string => address) private proxies_; // contentId => proxy
+  mapping (string => address) private proxyOwners_; // contentId => owner
+  mapping (string => address) public versions_; // version => implementation
+  mapping (address => address) public proxyImpls_; // proxy => implementation
 
   constructor() public {
     owner_ = msg.sender;
@@ -17,26 +19,52 @@ contract Registry {
      _;
   }
 
-  modifier onlyProxyOwner(string _contentId, address _address) {
-    address oldProxyAddress = proxies_[_contentId];
-    Proxy oldProxy = Proxy(oldProxyAddress);
-    Proxy newProxy = Proxy(_address);
-    if (newProxy.owner_() == msg.sender && (oldProxyAddress == address(0) || oldProxy.owner_() == newProxy.owner_())) _;
+  modifier onlyProxyOwner(string _contentId) {
+    require(proxyOwners_[_contentId] == msg.sender):
+    _;
   }
 
-  function getProxyAddress(string _contentId) external view returns (address) {
-    return proxies_[_contentId];
+  /**
+   * @dev AFS Proxy Factory
+   * @param _contentId The methodless content DID
+   * @param _version The implementation version to use with this Proxy
+   * @param _data AFS initialization data
+   */
+  function createAFS(string _contentId, string _version, bytes _data) public {
+    require(proxies_[_contentId] == address(0));
+    OwnedUpgradabilityProxy proxy = new OwnedUpgradabilityProxy(address(this));
+    upgradeProxy(_contentId, _version);
+    proxy.init(_data);
+    proxies_[_contentId] = proxy;
+    proxyOwners_[_contentId] = msg.sender;
   }
 
-  function addProxyAddress(string _contentId, address _address) public onlyProxyOwner(_contentId, _address) {
-    Proxy proxy = Proxy(_address);
-    address standard = proxy.implementation(); // call
-    assert(standard == standard_);
-    proxies_[_contentId] = _address;
+  /**
+   * @dev Upgrades proxy implementation version
+   * @param _contentId The methodless content DID
+   * @param _version The implementation version to upgrade this Proxy to
+   */
+  function upgradeProxy(string _contentId, string _version) public onlyProxyOwner(_contentId) {
+    require(versions_[_version] != address(0));
+    Proxy proxy = Proxy(proxies_[_contentId]);
+    proxy.upgradeTo(versions_[_version]);
+    proxyImpls_[proxy] = versions_[_version];
+  }
+
+  /**
+   * @dev Upgrades proxy implementation version with initialization
+   * @param _contentId The methodless content DID
+   * @param _version The implementation version to upgrade this Proxy to
+   * @param _data AFS initialization data
+   */
+  function upgradeProxyAndCall(string _contentId, string _version, bytes data) public onlyProxyOwner(_contentId) {
+    require(versions_[_version] != address(0));
+    Proxy proxy = Proxy(proxies_[_contentId]);
+    proxy.upgradeToAndCall(versions_[_version], data);
+    proxyImpls_[proxy] = versions_[_version];
   }
 
   function addStandardVersion(string _version, address _address) public restricted {
-    standard_ = _address;
-    version_ = _version;
+    versions_[_version] = _address;
   }
 }
