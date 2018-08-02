13258ba6c327239feaeac1191c40ba3283998e36
Eric Jiang <eric@littlstar.com>
2018-08-02 10:38:55 -0400

    feat(): proxy testable

--- constants.js
+++ constants.js
@@ -2,10 +2,10 @@ module.exports = {
   kDidPrefix: 'did:',
   kAidPrefix: 'did:ara:',
   kOwnerSuffix: '#owner',
-  kAFSAddress: '0xbab15d629b84e2c9575da294ab24b227e15fa02f',
-  kLibraryAddress: '0x3c43493ca8e7bdfa174ee77a05bc44f1986fda41',
-  kRegistryAddress: '0xebf3c3b116fc39b05a4b6e24acb3c7c5e604d07b',
-  kARATokenAddress: '0xfa21e79ca2dfb3ab15469796069622903919159c',
+  kAFSAddress: '0xb33d8395557533c57a265c975554ecf165c42ca1',
+  kLibraryAddress: '0xe94fa3040d20a01a1b8f42de7c430a11c27e2524',
+  kRegistryAddress: '0x8602fb26775de7467769821ffa111d2f2a62f979',
+  kARATokenAddress: '0x928892ad7e2fd6745a5b37732d647904dc3d6d6a',
 
   kTempOwnerDid: 'did:ara:e3a808f2deba17c1dcbaf176b2c529cac80c71b8418fe123439a054f88ae2cd2',
   kTempAFSDid: 'did:ara:7bfec1111365428137902d0152681a80f4ce535242ea384481cfc432e3016192'
