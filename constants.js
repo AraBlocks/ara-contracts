93db23e1b5290de26f7dec0725bb00b506a09292
Eric Jiang <eric@littlstar.com>
2018-08-06 17:21:59 -0400

    feat(): can deploy proxies and standards!

--- constants.js
+++ constants.js
@@ -2,9 +2,9 @@ module.exports = {
   kDidPrefix: 'did:',
   kAidPrefix: 'did:ara:',
   kOwnerSuffix: '#owner',
-  kRegistryAddress: '0x2e6eed75f2de36eb2a7debcd1de4b377b9c4e3b4',
-  kLibraryAddress: '0x6edfcd3f2f023f86e6624807e6a325e72a1ecf80',
-  kARATokenAddress: '0xf40ae6171ed0d266d8c2a8349c349af142cfb267',
+  kRegistryAddress: '0x9e1fdccae3c720c9e897b696640ab517dfd9bf6b',
+  kLibraryAddress: '0x31e36aa3e0400d572ccae095e4dff181e660e0c3',
+  kARATokenAddress: '0xad0c2681dc5d72572e36d0d312680b0da12facb1',
   kAFSAddress: '0x380dd06d0dbede67ba6ed8a56fe40bbc99b6ff1a',
 
   kTempOwnerDid: 'did:ara:e3a808f2deba17c1dcbaf176b2c529cac80c71b8418fe123439a054f88ae2cd2',
