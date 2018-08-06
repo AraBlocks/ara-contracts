9b30f1037190140ab490022ed5c4d6059e99bebb
Eric Jiang <eric@littlstar.com>
2018-08-06 15:42:26 -0400

    feat(): deploy standard progress

--- constants.js
+++ constants.js
@@ -2,10 +2,10 @@ module.exports = {
   kDidPrefix: 'did:',
   kAidPrefix: 'did:ara:',
   kOwnerSuffix: '#owner',
-  kRegistryAddress: '0x3ba7c2578b59e0e1ccfee9a20d92f043c0e0b3e6',
-  kLibraryAddress: '0x057d2360abbe75f9fdf142f2cfe68cfc9a74ec12',
-  kARATokenAddress: '0xf426505ac145abe033fe77c666840063757be9cd',
-  kAFSAddress: '0x9d075ae44d859191c121d7522da0cc3b104b8837',
+  kRegistryAddress: '0x2e6eed75f2de36eb2a7debcd1de4b377b9c4e3b4',
+  kLibraryAddress: '0x6edfcd3f2f023f86e6624807e6a325e72a1ecf80',
+  kARATokenAddress: '0xf40ae6171ed0d266d8c2a8349c349af142cfb267',
+  kAFSAddress: '0x380dd06d0dbede67ba6ed8a56fe40bbc99b6ff1a',
 
   kTempOwnerDid: 'did:ara:e3a808f2deba17c1dcbaf176b2c529cac80c71b8418fe123439a054f88ae2cd2',
   kTempAFSDid: 'did:ara:7bfec1111365428137902d0152681a80f4ce535242ea384481cfc432e3016192'
