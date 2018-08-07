c0b328d77d27a226c7b4a79b838cac32d375c4b1
Eric Jiang <eric@littlstar.com>
2018-08-07 14:31:23 -0400

    feat(): added convenience setup cli cmd

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 /* eslint no-undef: "off" */
 
+const { kDefaultAddress } = require('../constants')
 const Library = artifacts.require('./Library.sol')
 const Token = artifacts.require('./ARAToken.sol')
 const Registry = artifacts.require('./Registry.sol')
 
 module.exports = (deployer) => {
-  deployer.deploy(Registry, { from: "0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64" })
+  deployer.deploy(Registry, { from: kDefaultAddress })
     .then(() =>
-      deployer.deploy(Library, Registry.address, { from: "0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64" })
+      deployer.deploy(Library, Registry.address, { from: kDefaultAddress })
         .then(() =>
-          deployer.deploy(Token, { from: "0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64" })))
+          deployer.deploy(Token, { from: kDefaultAddress })))
 }
