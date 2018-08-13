3b1a73e86fe9e6aedd6f45c398587567d81c567a
Eric Jiang <eric@littlstar.com>
2018-08-13 10:47:47 -0400

    chore(): remove rc

--- rc.js
+++
@@ -1,9 +0,0 @@
-const rc = require('ara-runtime-configuration')
-const { resolve } = require('path')
-const extend = require('extend')
-
-const defaults = () => ({
-  proxy: resolve('contracts', 'Proxy.sol')
-})
-
-module.exports = conf => rc(extend(true, {}, defaults(), conf))
