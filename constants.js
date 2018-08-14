f64fb6272e6d0c43d48dc4d1d3e4d9fb6ff2271d
Eric Jiang <eric@littlstar.com>
2018-08-14 17:28:03 -0400

    feat(): add addresses to constants

--- constants.js
+++ constants.js
@@ -2,9 +2,9 @@ module.exports = {
   kDidPrefix: 'did:',
   kAidPrefix: 'did:ara:',
   kOwnerSuffix: '#owner',
-  kRegistryAddress: '0x9e1fdccae3c720c9e897b696640ab517dfd9bf6b',
-  kLibraryAddress: '0x31e36aa3e0400d572ccae095e4dff181e660e0c3',
-  kARATokenAddress: '0xad0c2681dc5d72572e36d0d312680b0da12facb1',
+  kRegistryAddress: '0xf02246d6ee000e2a4faf51eb4bb12d2b3fb2e5c9',
+  kLibraryAddress: '0x9708c4868b2609a3ac135b583be06306ebe64c46',
+  kARATokenAddress: '0x71be32bedf74b4a1ee208e1fee2b8f2fd613f50b',
 
   kTempOwnerDid: 'did:ara:4e56ed331bb52ae28f18969982275bb156f81f4b23c22115e9ddd2b51b4c89c4',
   // private key: 0xa38a74ba8d89e22841f1d705c0ae0b3c8355da9d56a0f2192b477563e4ee3d07
