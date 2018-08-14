34bb143fa98bc25db3c0a74b0ec1b418047bec34
Eric Jiang <eric@littlstar.com>
2018-08-14 17:27:33 -0400

    feat(): add testnet provider

--- truffle.js
+++ truffle.js
@@ -5,6 +5,13 @@ module.exports = {
       port: 9545,
       network_id: '*',
       from: '0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64'
+    },
+    testnet: {
+      network_id: 1337,
+      host: '34.229.98.210',
+      port: 9152,
+      from: '0x7c4ebbdc0639554089e0d71fe507e63a01856834',
+      gas: 2000000
     }
   }
 }
