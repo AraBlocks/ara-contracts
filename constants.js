c263c169c40db7ccf70fca12c194f75647a3253b
Eric Jiang <eric@littlstar.com>
2018-08-14 10:40:23 -0400

    fix(): fix AFS standard and more cli progress

--- constants.js
+++ constants.js
@@ -9,5 +9,5 @@ module.exports = {
   kTempOwnerDid: 'did:ara:4e56ed331bb52ae28f18969982275bb156f81f4b23c22115e9ddd2b51b4c89c4',
   // private key: 0xa38a74ba8d89e22841f1d705c0ae0b3c8355da9d56a0f2192b477563e4ee3d07
   kDefaultAddress: '0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64',
-  kTempAFSDid: 'did:ara:7bfec1111365428137902d0152681a80f4ce535242ea384481cfc432e3016192'
+  kTempAFSDid: 'did:ara:4251e5b2e8739fd744703b5644de9e11cfcf3f7a277bf2262e9b25c76c3e0bb5'
 }
