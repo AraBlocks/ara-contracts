21691beab86f718ae3579abc5969bf5fe1a7b283
Eric Jiang <eric@littlstar.com>
2018-08-23 14:05:16 -0400

    feat(): deploy new standards and upgrade proxies working

--- constants.js
+++ constants.js
@@ -6,15 +6,10 @@ module.exports = {
   kLibraryAddress: '0x31e36aa3e0400d572ccae095e4dff181e660e0c3',
   kARATokenAddress: '0xad0c2681dc5d72572e36d0d312680b0da12facb1',
 
-  // private chain
-  // kRegistryAddress: '0xf02246d6ee000e2a4faf51eb4bb12d2b3fb2e5c9',
-  // kLibraryAddress: '0x9708c4868b2609a3ac135b583be06306ebe64c46',
-  // kARATokenAddress: '0x71be32bedf74b4a1ee208e1fee2b8f2fd613f50b',
-
   kTempOwnerDid: 'did:ara:4e56ed331bb52ae28f18969982275bb156f81f4b23c22115e9ddd2b51b4c89c4',
   // private key: 0xa38a74ba8d89e22841f1d705c0ae0b3c8355da9d56a0f2192b477563e4ee3d07
   kDefaultAddress: '0xe6ecdb604957700f7e3c3057672dfe1b3ad60d64',
-  kTempAFSDid: 'did:ara:6ef9abd37a7298510a668f821f0050f438e36e7bf914ff0d71ba1ad74ff10e8e'
+  kTempAFSDid: 'did:ara:9861d0eafe27e080c5804b8f3de1aa762af4b4eb69a91b38a4cf87afbb52d643'
 }
 
 // AFS standard: '0xBcf229176416A2E889DB0e7d78AC375fC4B28d68'
