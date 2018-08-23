b9b5ee6f705bf0aa8a0ab6baf1de73e57850485c
Eric Jiang <eric@littlstar.com>
2018-08-23 11:38:54 -0400

    feat(): allocate rewards working

--- constants.js
+++ constants.js
@@ -20,3 +20,22 @@ module.exports = {
 // AFS standard: '0xBcf229176416A2E889DB0e7d78AC375fC4B28d68'
 // Proxy: '0xBAc53704b34710587ABCb042b69D7C627F8cF263'
 
+/**
+ * FARMERS:
+ *  (1)     DID: did:ara:a51aa651c5a28a7c0a8de007843a00dcd24f3cc893522d3fb093c2bb7a323785
+ *      ADDRESS: 0xF9403C6DA32DB4860F1eCB1c02B9A04D37c0e36e
+ *          KEY: 0xc169a154b069eabe0bb54cf1bc4abe100e34bcd74d0e32d16d87c00033292b46
+ *     MNEMONIC: < offer loop foil panic hobby subway runway deny canyon impose speed ride >
+ *  (2)     DID: did:ara:03e5a1ecc9e7c4639dfb3e0d90302c77b51ca083dfaecb64af1c8f15c9fc0683
+ *      ADDRESS: 0x70693d8f4e1c9bA1AE0870C35128BaDfDcF28FBc
+ *          KEY: 0xb6313da9a02485ad500392d4955017d98794095c666723c463c94d3cb8c6ed52
+ *     MNEMONIC: < stock office soft harvest path joy tail alpha topple pen village music >
+ *  (3)     DID: did:ara:067cfd3d2e56e80838b59247d1589b9180cc274a8e7c0a930c7d4f8ecf5e95f0
+ *      ADDRESS: 0x19d6a7D8bB09e8A6d733a9c8D9fe7b964fD8F45e
+ *          KEY: 0x69b91242b15c8f1dcd72cee1a1dbf5be08c0b0db008f819819cebea0ca24bf6e
+ *     MNEMONIC: < grant wild either uncover ivory release beyond jungle believe drift topic clever >
+ *  (4)     DID: did:ara:e5e6946e25d6c7e4fd0eb716f0361d42a5bcfac663e55f08ab539f4ba0a6faba
+ *      ADDRESS: 0x629483C72b5191C1b522E887238a0A522b1D4F74
+ *          KEY: 0x32b1acdfc044649631bb22821f661ea5e9dba65e98637f1fa2ec3c3056582acc
+ *     MNEMONIC: < snap poet general square silk ranch whip lizard faculty upon cup degree >
+ */
