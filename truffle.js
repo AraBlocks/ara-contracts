72f0f4cb8d67dd626faf49b3d3ea3d63332afdfa
cckelly21 <cckelly21@gmail.com>
2018-09-14 13:27:55 -0400

    chore(): testing truffle running in separate process for travis

--- truffle.js
+++ truffle.js
@@ -1,18 +1,9 @@
 module.exports = {
   networks: {
-    testnet: {
-      network_id: 1337,
-      host: '34.229.98.210',
-      port: 9151,
-      from: '0x7c4ebbdc0639554089e0d71fe507e63a01856834',
-      gas: 2000000
-    },
-    localnet: {
-      network_id: 12,
+    local: {
+      network_id: '*',
       host: 'localhost',
-      port: 9546,
-      from: '0x105c83b79e9170d7969ce9806fe0b527b5f879de',
-      gas: 7000000
+      port: 8545
     },
     privatenet: {
       network_id: 1337,
