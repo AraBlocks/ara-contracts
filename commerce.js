4bff9c23482482e3e48491192d292deb3b994782
cckelly21 <cckelly21@gmail.com>
2018-10-03 13:15:31 -0400

    feat(): refactored Ownable to allow for staging transfers

--- commerce.js
+++ commerce.js
@@ -7,7 +7,6 @@ const {
   web3: {
     isAddress,
     account,
-    call,
     tx
   }
 } = require('ara-util')
@@ -17,7 +16,64 @@ const {
   proxyExists
 } = require('./registry')
 
-async function transferOwnership(opts) {
+async function approveOwnershipTransfer(opts) {
+  if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('Expecting opts object')
+  } else if (!opts.did || 'string' !== typeof opts.did) {
+    throw new TypeError('Expecting non-empty staged owner DID')
+  } else if (!opts.password || 'string' !== typeof opts.password) {
+    throw new TypeError('Expecting non-empty password')
+  } else if (!opts.contentDid || 'string' !== typeof opts.contentDid) {
+    throw new TypeError('Expecting non-empty content DID')
+  } else if (opts.estimate && 'boolean' !== typeof opts.estimate) {
+    throw new TypeError('Expecting boolean for estimate')
+  }
+
+  const {
+    did,
+    password,
+    contentDid,
+  } = opts
+
+  let stagedOwnerAddress
+  try {
+    await validate({ did, password, label: 'approveOwnershipTransfer' })
+    stagedOwnerAddress = await getAddressFromDID(normalize(did))
+  } catch (err) {
+    throw err
+  }
+
+  if (!(await proxyExists(contentDid))) {
+    throw new Error('Content does not have a valid proxy contract')
+  }
+
+  if (!isAddress(stagedOwnerAddress)) {
+    throw new Error(`${did} did not resolve to a valid Ethereum address. 
+      Got ${stagedOwnerAddress}. Ensure ${did} is a valid Ara identity.`)
+  }
+
+  const proxy = await getProxyAddress(contentDid)
+  const acct = await account.load({ did, password })
+  const approveTx = await tx.create({
+    account: acct,
+    to: proxy,
+    gasLimit: 1000000,
+    data: {
+      abi,
+      functionName: 'approveOwnershipTransfer'
+    }
+  })
+
+  const estimate = opts.estimate || false
+
+  if (estimate) {
+    return tx.estimateCost(approveTx)
+  }
+
+  return tx.sendSignedTransaction(approveTx)
+}
+
+async function stageOwnershipTransfer(opts) {
   if (!opts || 'object' !== typeof opts) {
     throw new TypeError('Expecting opts object')
   } else if (!opts.ownerDid || 'string' !== typeof opts.ownerDid) {
@@ -42,13 +98,13 @@ async function transferOwnership(opts) {
   let ownerAddress
   let newOwnerAddress
   try {
-    await validate({ did: ownerDid, password, label: 'transferOwnership' })
+    await validate({ did: ownerDid, password, label: 'stageOwnershipTransfer' })
     ownerAddress = await getAddressFromDID(normalize(ownerDid))
     newOwnerAddress = await getAddressFromDID(normalize(newOwnerDid))
   } catch (err) {
     throw err
   }
-  
+
   if (!(await proxyExists(contentDid))) {
     throw new Error('Content does not have a valid proxy contract')
   }
@@ -65,33 +121,27 @@ async function transferOwnership(opts) {
 
   const proxy = await getProxyAddress(contentDid)
   const acct = await account.load({ did: ownerDid, password })
-  const transferTx = await tx.create({
+  const stageTx = await tx.create({
     account: acct,
     to: proxy,
     gasLimit: 1000000,
     data: {
       abi,
-      functionName: 'transferOwnership',
+      functionName: 'stageOwnershipTransfer',
       values: [ newOwnerAddress ]
     }
   })
 
-  const owner = await call({
-    abi,
-    address: proxy,
-    functionName: 'owner'
-  })
-  debug('current AFS owner', owner)
-
   const estimate = opts.estimate || false
 
   if (estimate) {
-    return tx.estimateCost(transferTx)
+    return tx.estimateCost(stageTx)
   }
 
-  return tx.sendSignedTransaction(transferTx)
+  return tx.sendSignedTransaction(stageTx)
 }
 
 module.exports = {
-  transferOwnership
+  approveOwnershipTransfer,
+  stageOwnershipTransfer
 }
