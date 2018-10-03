00c76ce93c7637fc02ad4c6288572ddd510f76c8
cckelly21 <cckelly21@gmail.com>
2018-10-03 15:07:19 -0400

    fix(contracts/Ownable.sol): remove internal functions

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -19,20 +19,12 @@ contract Ownable {
   }
 
   function stageOwnershipTransfer(address _stagedOwner) public onlyOwner {
-    _stageOwnershipTransfer(_stagedOwner);
-  }
-
-  function approveOwnershipTransfer() public onlyStagedOwner {
-    _approveOwnershipTransfer();
-  }
-
-  function _stageOwnershipTransfer(address _stagedOwner) internal {
     require(_stagedOwner != address(0));
     emit TransferStaged(owner_, _stagedOwner);
     stagedOwner_ = _stagedOwner;
   }
 
-  function _approveOwnershipTransfer() internal {
+  function approveOwnershipTransfer() public onlyStagedOwner {
     require(stagedOwner_ != address(0));
     emit TransferApproved(owner_, stagedOwner_);
     owner_ = stagedOwner_;
