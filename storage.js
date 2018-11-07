e2221475a1ff1ede1eddb53c0735f93c4aa11624
cckelly21 <cckelly21@gmail.com>
2018-11-07 13:19:15 -0500

    refactor(storage.js): move hasBuffer to storage

--- storage.js
+++ storage.js
@@ -2,18 +2,7 @@ const { web3: { tx, call, isAddress } } = require('ara-util')
 const { abi } = require('./build/contracts/AFS.json')
 
 async function read(opts) {
-  if (!opts || 'object' !== typeof opts) {
-    throw new TypeError('Expecting opts to be of type object')
-  } else if (!opts.address || 'string' !== typeof opts.address || !isAddress(opts.address)) {
-    throw new TypeError('Expecting opts.address to be valid Ethereum address')
-  } else if (!Object.prototype.hasOwnProperty.call(opts, 'fileIndex')
-      || 'number' !== typeof opts.fileIndex) {
-    throw new TypeError('Expecting opts.fileIndex to be of type number')
-  } else if (!Object.prototype.hasOwnProperty.call(opts, 'offset')
-      || 'number' !== typeof opts.offset) {
-    throw new TypeError('Expecting opts.offset to be of type number')
-  }
-
+  _validateOpts(opts)
   const { address, fileIndex, offset } = opts
 
   return call({
@@ -67,7 +56,48 @@ async function write(opts, estimate = true, append = false) {
   return tx.sendSignedTransaction(transaction)
 }
 
+async function hasBuffer(opts) {
+  _validateOpts(opts)
+
+  if (!opts.buffer || 'string' !== typeof opts.buffer) {
+    throw new TypeError('Expecting valid hex string for opts.buffer')
+  }
+
+  const {
+    fileIndex,
+    address,
+    offset,
+    buffer
+  } = opts
+
+  return call({
+    abi,
+    address,
+    functionName: 'hasBuffer',
+    arguments: [
+      fileIndex,
+      offset,
+      buffer
+    ]
+  })
+}
+
+function _validateOpts(opts) {
+  if (!opts || 'object' !== typeof opts) {
+    throw new TypeError('Expecting opts to be of type object')
+  } else if (!opts.address || 'string' !== typeof opts.address || !isAddress(opts.address)) {
+    throw new TypeError('Expecting opts.address to be valid Ethereum address')
+  } else if (!Object.prototype.hasOwnProperty.call(opts, 'fileIndex')
+      || 'number' !== typeof opts.fileIndex) {
+    throw new TypeError('Expecting opts.fileIndex to be of type number')
+  } else if (!Object.prototype.hasOwnProperty.call(opts, 'offset')
+      || 'number' !== typeof opts.offset) {
+    throw new TypeError('Expecting opts.offset to be of type number')
+  }
+}
+
 module.exports = {
+  hasBuffer,
   write,
   read
 }
