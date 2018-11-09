f3f83beb104397785bbb9d31ba8760a14fed2f38
Eric Jiang <eric@littlstar.com>
2018-11-09 16:30:51 -0500

    fix(): close context after estimate

--- registry.js
+++ registry.js
@@ -220,12 +220,10 @@ async function deployProxy(opts) {
 
   let proxyAddress = null
   try {
-    debug('before encode')
     const encodedData = web3Abi.encodeParameters(
       [ 'address', 'address', 'address', 'bytes32' ],
       [ acct.address, ARA_TOKEN_ADDRESS, LIBRARY_ADDRESS, toHexString(contentDid, { encoding: 'hex', ethify: true }) ]
     )
-
     debug('encoded tx')
     const { tx: transaction, ctx: ctx1 } = await tx.create({
       account: acct,
@@ -241,7 +239,6 @@ async function deployProxy(opts) {
         ]
       }
     })
-    debug('created tx')
     if (estimate) {
       const cost = tx.estimateCost(transaction)
       ctx1.close()
@@ -363,15 +360,12 @@ async function deployNewStandard(opts) {
   }
 
   const prefixedDid = `${AID_PREFIX}${did}`
-  debug('load account')
   const acct = await account.load({ did: prefixedDid, password })
-  debug('get reg owner')
   const registryOwner = await call({
     abi,
     address: REGISTRY_ADDRESS,
     functionName: 'owner_'
   })
-  debug('got reg owner')
   if (acct.address != registryOwner) {
     throw new Error('Only the owner of the Registry contract may deploy a new standard.')
   }
@@ -403,7 +397,6 @@ async function deployNewStandard(opts) {
       abi: afsAbi,
       bytecode: toHexString(bytecode, { encoding: 'hex', ethify: true })
     })
-    debug('creating tx')
     const { tx: transaction, ctx: ctx1 } = await tx.create({
       account: acct,
       to: REGISTRY_ADDRESS,
@@ -417,7 +410,6 @@ async function deployNewStandard(opts) {
         ]
       }
     })
-    debug('created tx')
     // listen to ProxyDeployed event for proxy address
     const { contract: registry, ctx: ctx2 } = await contract.get(abi, REGISTRY_ADDRESS)
     address = await new Promise((resolve, reject) => {
