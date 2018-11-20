c44c43049184e5da6df3438e2bc03ab5ddf14ab9
Eric Jiang <eric@littlstar.com>
2018-11-19 19:02:33 -0500

    fix(): travis

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -33,9 +33,9 @@ module.exports = (deployer, network, defaultAccounts) => {
       const { web3 } = ctx
       const accounts = await web3.eth.getAccounts()
       if (!accounts.includes(constants.TEST_OWNER_ADDRESS)) {
-        await web3.eth.personal.importRawKey(constants.TEST_OWNER_PK, constants.PASSWORD)
+        await web3.eth.personal.importRawKey(constants.TEST_OWNER_PK, constants.OWNER_PASSWORD)
       }
-      await web3.eth.personal.unlockAccount(constants.TEST_OWNER_ADDRESS, constants.PASSWORD, 0)
+      await web3.eth.personal.unlockAccount(constants.TEST_OWNER_ADDRESS, constants.OWNER_PASSWORD, 0)
       await web3.eth.sendTransaction({ from: accounts[0], to: constants.TEST_OWNER_ADDRESS, value: web3.utils.toWei('1', 'ether') })
       await web3.eth.sendTransaction({ from: accounts[0], to: constants.TEST_OWNER_ADDRESS_2, value: web3.utils.toWei('1', 'ether') })
       ctx.close()
