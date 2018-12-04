7d90f9e627c01fae810b0a306c11c5c9374458f8
Eric Jiang <eric@littlstar.com>
2018-12-04 17:53:07 -0500

    fix(): lint

--- test/library.js
+++ test/library.js
@@ -5,8 +5,7 @@ const test = require('ava')
 const {
   TEST_OWNER_DID_NO_METHOD,
   TEST_AFS_DID1,
-  TEST_AFS_DID2,
-  PASSWORD: password
+  TEST_AFS_DID2
 } = require('./_constants')
 
 const {
@@ -37,12 +36,12 @@ test.serial('hasPurchased(opts)', async (t) => {
   const purchaserDid = getDid(t)
   const contentDid = getAfsDid(t)
 
-  t.true(await library.hasPurchased({ purchaserDid, contentDid })) // contentDid is purchased in purchase.js tests
+  // contentDid is purchased in purchase.js tests
+  t.true(await library.hasPurchased({ purchaserDid, contentDid }))
 })
 
 test.serial('hasPurchased(opts) invalid opts', async (t) => {
   const purchaserDid = getDid(t)
-  const contentDid = getAfsDid(t)
 
   await t.throwsAsync(library.hasPurchased(), TypeError)
   await t.throwsAsync(library.hasPurchased({ }), TypeError)
@@ -65,7 +64,9 @@ test.serial('hasPurchased(opts) invalid opts', async (t) => {
 test.serial('getLibrarySize(requesterDid)', async (t) => {
   const requesterDid = getDid(t)
   const size = await library.getLibrarySize(requesterDid)
-  t.is(size, '2') // Two AFSs are purchased in purchase.js tests
+
+  // Two AFSs are purchased in purchase.js tests
+  t.is(size, '2')
 })
 
 test.serial('getLibrarySize(requesterDid) invalid opts', async (t) => {
@@ -79,13 +80,13 @@ test.serial('getLibrarySize(requesterDid) invalid opts', async (t) => {
 test.serial('getLibraryItem(opts)', async (t) => {
   const requesterDid = getDid(t)
 
-  t.is(await library.getLibraryItem({ requesterDid, index: 0 }), toHexString(TEST_AFS_DID1, { encoding: 'hex', ethify: true}))
-  t.is(await library.getLibraryItem({ requesterDid, index: 1 }), toHexString(TEST_AFS_DID2, { encoding: 'hex', ethify: true}))
+  t.is(await library.getLibraryItem({ requesterDid, index: 0 }), toHexString(TEST_AFS_DID1, { encoding: 'hex', ethify: true }))
+  t.is(await library.getLibraryItem({ requesterDid, index: 1 }), toHexString(TEST_AFS_DID2, { encoding: 'hex', ethify: true }))
 })
 
 test.serial('getLibraryItem(opts) invalid opts', async (t) => {
   const requesterDid = getDid(t)
-  
+
   await t.throwsAsync(library.getLibraryItem(), TypeError)
   await t.throwsAsync(library.getLibraryItem({ }), TypeError)
   await t.throwsAsync(library.getLibraryItem([ ]), TypeError)
