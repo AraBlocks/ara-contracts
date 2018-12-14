9392b4cc83fa66552536a8b8c5162adc75a97972
Eric Jiang <eric@littlstar.com>
2018-12-14 18:44:36 -0500

    feat(): wip

--- contracts/AraFactory.sol
+++ contracts/AraFactory.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
 contract AraFactory {
-  event ContractDeployed(address _deployedAddress);
+  event ContractDeployed(String _label, address _deployedAddress);
 
-  function deployContract(bytes _code) returns (address deployedAddress) {
+  function deployContract(String label, bytes _code) returns (address deployedAddress) {
     assembly {
       deployedAddress := create(0, add(_code, 0x20), mload(_code))
       jumpi(invalidJumpLabel, iszero(extcodesize(deployedAddress))) // jumps if no code at addresses
     }
-    ContractDeployed(deployedAddress);
+    ContractDeployed(label, deployedAddress);
   }
 }
