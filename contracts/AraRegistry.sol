fa222345ca0140ae03029d05d0c7fc52e0f9eb0d
Eric Jiang <eric@littlstar.com>
2019-01-10 12:40:00 -0500

    feat(): API WIP

--- contracts/AraRegistry.sol
+++ contracts/AraRegistry.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "./AraProxy.sol";
+import "./ignored_contracts/AraProxy.sol";
 
 contract AraRegistry {
   mapping (string => UpgradeableContract) private contracts_; // contract name => struct
